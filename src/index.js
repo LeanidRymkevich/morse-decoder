ad3a7944708fa833b619d298f8609a0e2ff21094
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-01-22 15:04:42 +0300

    feat: adding the decoder to morse from binary code for symbols

--- src/index.js
+++ src/index.js
@@ -41,6 +41,53 @@ function decode(expr) {
     // write your solution here
 }
 
+function stringToTensArray(string){
+    let binary_letters = [];
+  
+    for(let i = 0; i < string.length; i += 10){
+      binary_letters.push(string.slice(i, i + 10));
+    }
+  
+    return binary_letters;
+}
+
+function binaryLetterToMorse(string){
+    const space = {
+      binary_code : '*'.repeat(10),
+      morse_code : ' ',
+    };
+    const dot = {
+      binary_code : '10',
+      morse_code : '.',
+    }
+    const dash = {
+      binary_code : '11',
+      morse_code : '-',
+    };
+  
+    let result = '';
+  
+    if(string === space.binary_code){
+      return space.morse_code;
+    }
+  
+    for(let i = string.length; i > 0; i -= 2){
+      let substring = string.slice(i-2,i);
+      if(substring === dot.binary_code){
+        result = dot.morse_code + result;
+      }
+      if(substring === dash.binary_code){
+        result = dash.morse_code + result;
+      }
+      if(substring === '00'){
+        continue;
+      }
+    }
+    return result;
+  }
+
+
+
 module.exports = {
     decode
 }
